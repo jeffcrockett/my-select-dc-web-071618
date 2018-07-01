78ff834d1e5dc76a629e3d286e72aa3b05d31ab6
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-01 18:29:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  new_array = [] 
  index = 0 
  while index < collection.length 
-  new_val = yield array[index]
+  new_val = yield collection[index]
   if new_val
     new_array << new_val
   end
